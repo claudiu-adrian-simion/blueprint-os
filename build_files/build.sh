374c03bbf943d5fabc48a5392a5cebe16a7d8f0e
claudiu-adrian-simion <claudiuadrian.simion@gmail.com>
2026-09-03 16:22:00 +0200

    FIX zram conflict

--- build_files/build.sh
+++ build_files/build.sh
@@ -31,6 +31,7 @@ dnf5 -y copr disable bieszczaders/kernel-cachyos
 
 ### Install CachyOS kernel addons
 dnf5 -y copr enable bieszczaders/kernel-cachyos-addons
+dnf5 -y swap zram-generator-defaults cachyos-settings
 dnf5 -y install cachyos-settings scx-scheds
 dnf5 -y copr disable bieszczaders/kernel-cachyos-addons
 
